bb7b70921125ee05e85072cdc0acde007c452ff4
Thu Ta <thutazaw.ttz92@gmail.com>
2021-10-07 19:57:42 +0630

    dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,54 +1,55 @@
 @extends('blog.master')
 @section('content')
     <div class="article-list">
-            @forelse($articles as $article)
-                <div class="border-bottom mb-4 pb-4 article-preview">
-                    <div class="p-0 p-md-3">
-                        <a class="fw-bold h4 d-block text-decoration-none"
-                           href="{{ route('detail',$article->id) }}">
-                            {{ $article->title }}</a>
+        @forelse($articles as $article)
+            <div class="border-bottom mb-4 pb-4 article-preview">
+                <div class="p-0 p-md-3">
+                    <a class="fw-bold h4 d-block text-decoration-none" href="{{ route('detail', $article->id) }}">
+                        {{ $article->title }}</a>
 
-                        <div class="small post-category mb-3">
-                            <a href="{{ route('baseOnCategory',$article->category->id ) }}" rel="category tag">{{ $article->category->title }}</a>
-                        </div>
+                    <div class="small post-category mb-3">
+                        <a href="{{ route('baseOnCategory', $article->category->id) }}"
+                            rel="category tag">{{ $article->category->title }}</a>
+                    </div>
 
-                        @isset($article->feature_image)
+                    @isset($article->feature_image)
                         <div class="my-3 feature-image-box">
-                            <img width="1024" height="682" src="{{asset('storage/articles/'.$article->feature_image)}}" class="attachment-large size-large wp-post-image" alt=""></div>
-                        @endisset
-                        
-
-                        <div class="text-black-50 the-excerpt">
-                            <p>{{ Str::words($article->description,50) }}</p>
+                            <img width="1024" height="682" src="{{ asset('storage/articles/' . $article->feature_image) }}"
+                                class="attachment-large size-large wp-post-image" alt="">
                         </div>
+                    @endisset
 
-                        <div class="d-flex justify-content-between align-items-center see-more-group">
-                            <div class="d-flex align-items-center">
-                                @if($article->user->photo)
-                                    <img alt="" src="{{ asset('storage/profile/'.$article->user->photo) }}"
-                                         class="avatar avatar-50 photo rounded-circle" height="50" width="50"
-                                    >
-                                @else
-                                    <img alt="" src="{{ asset('dashboard/img/user-default-photo.png') }}"
-                                         class="avatar avatar-50 photo rounded-circle" height="50" width="50"
-                                    >
-                                @endif
 
-                                <div class="ms-2">
-                                        <span class="small">
-                                            <i class="feather-user"></i>
-                                            {{ $article->user->name }}
-                                        </span>
-                                    <br>
-                                    <span class="small">{{ $article->created_at->format("d F Y") }}</span>
-                                </div>
+                    <div class="text-black-50 the-excerpt">
+                        <p>{{ Str::words($article->description, 50) }}</p>
+                    </div>
+
+                    <div class="d-flex justify-content-between align-items-center see-more-group">
+                        <div class="d-flex align-items-center">
+                            @if ($article->user->photo)
+                                <img alt="" src="{{ asset('storage/profile/' . $article->user->photo) }}"
+                                    class="avatar avatar-50 photo rounded-circle" height="50" width="50">
+                            @else
+                                <img alt="" src="{{ asset('dashboard/img/user-default-photo.png') }}"
+                                    class="avatar avatar-50 photo rounded-circle" height="50" width="50">
+                            @endif
+
+                            <div class="ms-2">
+                                <span class="small">
+                                    <i class="feather-user"></i>
+                                    {{ $article->user->name }}
+                                </span>
+                                <br>
+                                <span class="small">{{ $article->created_at->format('d F Y') }}</span>
                             </div>
-
-                            <a href="{{ route('detail',$article->id) }}" class="btn btn-outline-primary rounded-pill px-3">Read More</a>
                         </div>
+
+                        <a href="{{ route('detail', $article->id) }}"
+                            class="btn btn-outline-primary rounded-pill px-3">Read More</a>
                     </div>
                 </div>
-            @empty
+            </div>
+        @empty
 
             <div class="mb-4 pb-4">
                 <div class="py-5 my-5 text-center text-lg-start">
@@ -80,9 +81,8 @@
 
 @endsection
 
-@section("pagination-place")
+@section('pagination-place')
     <div class="d-none d-lg-block" id="pagination">
         {{ $articles->onEachSide(1)->links() }}
     </div>
-    @stop
-
+@stop
